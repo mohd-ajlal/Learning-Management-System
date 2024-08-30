b3a40cec8f755c3b195dd36ee1f75643a24aa665
Mohd Ajlal <ajlal00786@gmail.com>
2024-08-31 03:19:56 +0530

    create course

--- server/app.ts
+++ server/app.ts
@@ -1,44 +1,52 @@
 require("dotenv").config();
-import express, { NextFunction, Request, Response } from "express"
+import express, { NextFunction, Request, Response } from "express";
 export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import {ErrorMiddleware} from "./middleware/error"
+import { ErrorMiddleware } from "./middleware/error";
 import userRouter from "./router/user.route";
-
-
+import courseRouter from "./router/course.route";
 
 // body parser
-app.use(express.json({limit:"50mb"}));
+app.use(express.json({ limit: "50mb" }));
 
 // cookie parser
 app.use(cookieParser());
 
 // cors => cross origin resource sharing
-app.use(cors({
-    origin: process.env.ORIGIN
-}));
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
 
 // routes
 
-app.use("/api/v1",userRouter);
+app.use("/api/v1", userRouter);
+app.use("/api/v1", courseRouter);
 
 // testing api
 
-app.get("/test", (req:Request,res:Response, next:NextFunction)=>{
-    res.status(200).json({
-        success:true,
-        message:"API is working fine"
-    })
-})
-
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "API is working fine",
+  });
+});
 
 // unknown route
 
-app.all("*", (req:Request,res:Response<any, Record<string, any>>, next:NextFunction)=>{
+app.all(
+  "*",
+  (
+    req: Request,
+    res: Response<any, Record<string, any>>,
+    next: NextFunction
+  ) => {
     const err = new Error(`Route ${req.originalUrl} not found`) as any;
     err.statusCode = 400;
     next(err);
-})
+  }
+);
 
 app.use(ErrorMiddleware);

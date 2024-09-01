d19fd333cb52f94b69b6c60891291827b97f27f3
Mohd Ajlal <ajlal00786@gmail.com>
2024-09-01 23:00:38 +0530

    course create question

--- server/models/course.model.ts
+++ server/models/course.model.ts
@@ -3,8 +3,8 @@ import { title } from "process";
 
 interface IComment extends Document {
   user: object;
-  comment: string;
-  commentReplies?: IComment[];
+  question: string;
+  questionReplies?: IComment[];
 }
 
 interface IReview extends Document {
@@ -71,13 +71,13 @@ const linkSchema = new Schema<ILink>({
 
 const commentSchema = new Schema<IComment>({
   user: Object,
-  comment: String,
-  commentReplies: [Object],
+  question: String,
+  questionReplies: [Object],
 });
 
 const courseDataSchema = new Schema<ICourseData>({
   videoUrl: String,
-//   videoThumbnail: Object,
+  videoThumbnail: Object,
   title: String,
   videoSection: String,
   description: String,
--- server/router/course.route.ts
+++ server/router/course.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../controllers/course.controller";
+import { addQuestion, editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../controllers/course.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 
 const courseRouter = express.Router();
@@ -28,10 +28,16 @@ courseRouter.get(
     getAllCourses
   );
 
-  courseRouter.get(
+courseRouter.get(
     "/get-course-content/:id",
     isAuthenticated,
     getCourseByUser
+  );
+
+courseRouter.put(
+    "/add-question",
+    isAuthenticated,
+    addQuestion
   );
 
 export default courseRouter;

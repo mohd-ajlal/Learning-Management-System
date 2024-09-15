4312476a86b1ff06a208a73f05756c1462bc88e2
Mohd Ajlal <ajlal00786@gmail.com>
2024-09-15 18:51:59 +0530

    get all notifications || admin

---
+++ server/controllers/notification.controller.ts
@@ -0,0 +1,23 @@
+import NotificationModel from "../models/notification.model";
+import { NextFunction, Request, Response } from "express";
+import { CatchAsyncError } from "../middleware/catchAsyncErrors";
+import ErrorHandler from "../utils/ErrorHandler";
+
+
+// get all notifications on admin dashboard
+export const getNotifications = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const notifications = await NotificationModel.find().sort({
+        createdAt: -1,
+      });
+
+      res.status(201).json({
+        success: true,
+        notifications,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
